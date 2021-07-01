5346e3c7e4d043a69c1ba4449c9e6fccde0be44b
wonlee6 <wonlee6@gmail.com>
2021-07-01 19:20:57 +0900

    리스트 아이템 만들기

--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import VideoItem from "../video_item/video_item";
+import styles from "./video_list.module.css";
 
 const Video_list = (props) => {
   return (
-    <ul>
+    <ul className={styles.video}>
       {props.videos.map((video) => (
         <VideoItem key={video.id} video={video} />
       ))}
